1b7662047f42f5d7c64baa231f6066235f06873e
Yury G <golovanovya@gmail.com>
2019-03-03 22:36:17 +0300

    add fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,31 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+    
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager)
     {
         // $product = new Product();
         // $manager->persist($product);
-
+        
+        $user = new \App\Entity\User();
+        $user->setUsername('admin');
+        $user->setRoles(['ROLE_ADMIN']);
+        $password = $this->encoder->encodePassword($user, 'admin');
+        $user->setPassword($password);
+        $manager->persist($user);
+        
         $manager->flush();
     }
 }
